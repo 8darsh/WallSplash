f358dca40e3f64771859bd78046cbed6a93f5551
Adarsh <86889416+8darsh@users.noreply.github.com>
2023-10-10 16:31:19 +0530

    Initial Commit

---
+++ WallSplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallSplash
+//
+//  Created by Adarsh Singh on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
